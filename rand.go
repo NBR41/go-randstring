7ddcb6bc49d19859d8e8b27ef8ea5a5e1aaecbfb
fbernard <fabien.bernard@twenga.com>
2016-12-12 10:54:12 +0100

    add random string

---
+++ rand.go
@@ -0,0 +1,40 @@
+package randstring
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Get returns a random stirng of length n
+// http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+func Get(n int) string {
+
+	b := make([]byte, n)
+
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(charBytes) {
+			b[i] = charBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
